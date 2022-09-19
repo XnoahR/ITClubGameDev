ce835e0d435ac8c10c8b04a803bf852c59aaca9f
Swordigo15 <67177319+Swordigo15@users.noreply.github.com>
2022-09-19 20:17:50 +0700

    Update Player.cs

--- ScriptMaterial/Player.cs
+++ ScriptMaterial/Player.cs
@@ -6,16 +6,19 @@ public class Player : MonoBehaviour
 {
   Rigidbody2D rb;
   [SerializedField] int speed;
+  [SerializedField] int jumpSpeed;
+  [SerializedField] bool isJump = false;
+  
   void Start(){ //Function yang akan dipanggil sekali di awal
     rb = GetComponent<Rigidbody2D>();
   }
   
   void Update(){ //Function yang dipanggil sekali tiap frame
     float x = Input.GetAxisRaw("Horizontal");
-    rb.velocity = Vector2(x * 5 * Time.deltaTime, velocity.y);
+    rb.velocity = Vector2(x * 5 * speed * Time.deltaTime, rb.velocity.y);
     
-    if(Input.GetKey(KeyCode.Space)){
-      rb.AddForce(new Vector2(0, 5 * speed * Time.deltaTime));
+    if(Input.GetKeyDown(KeyCode.Space) && isJump == false){
+      rb.AddForce(new Vector2(0, 50 * jumpSpeed * Time.deltaTime));
     }
     /*if(Input.GetKey(KeyCode.D)){
       rb.AddForce(Vector2.right);
@@ -23,5 +26,17 @@ public class Player : MonoBehaviour
     if(Input.GetKey(KeyCode.A)){
       rb.AddForce(Vector2.left);
     }*/
+  }
+  
+  public void OnCollisionEnter2D(Collision2D other){
+    if(other.gameObject.tag == "lantai"){
+      isJump = false;
+    }
+  }
+  
+  public void OnCollisionExit2D(Collision2D other){
+    if(other.gameObject.tag == "lantai"){
+      isJump = true;
+    }
   }
 }
