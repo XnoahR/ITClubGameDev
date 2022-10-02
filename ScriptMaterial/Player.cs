0a0151c4c5c97e3baae299def359c7a07e660de2
Rio Saputro <95954236+XnoahR@users.noreply.github.com>
2022-10-03 01:20:06 +0700

    Weapon Update

--- ScriptMaterial/Player.cs
+++ ScriptMaterial/Player.cs
@@ -1,49 +1,61 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-  Rigidbody2D rb;
-  [SerializedField] int speed;
-  [SerializedField] int jumpSpeed;
-  [SerializedField] bool isJump = false;
-  
-  void Start(){ //Function yang akan dipanggil sekali di awal
-    rb = GetComponent<Rigidbody2D>();
-  }
-  
-  void Update(){ //Function yang dipanggil sekali tiap frame
-    float x = Input.GetAxisRaw("Horizontal");
-    rb.velocity = Vector2(x * 5 * speed * Time.deltaTime, rb.velocity.y);
-    
-    if(Input.GetKeyDown(KeyCode.Space) && isJump == false){
-      rb.AddForce(new Vector2(0, 50 * jumpSpeed * Time.deltaTime));
+    Rigidbody2D rb;
+    [SerializeField] int speed;
+    public int JumpSpeed;
+    public bool isJump = false;
+    public GameObject FirePoint;
+    public bool FacingRight = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
     }
-    
-    if(x > 0.01f){
-      transform.localScale = Vector2.one;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        float x = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(x*5*speed*Time.fixedDeltaTime,rb.velocity.y);
+
+        if(Input.GetKeyDown(KeyCode.Space) && isJump == false){
+            rb.AddForce(new Vector2(0,50*JumpSpeed*Time.deltaTime));
+        }
+        if(x > 0.01f && FacingRight){
+            transform.localScale = Vector2.one;
+            Flip();
+            
+        }
+        else if(x < -0.01f && !FacingRight){
+            transform.localScale = new Vector2(-1,1);
+            Flip();
+        }
+        /*if(Input.GetKey(KeyCode.D)){
+            rb.AddForce(Vector2.right);
+        }
+        if(Input.GetKey(KeyCode.A)){
+            rb.AddForce(Vector2.left);
+        }
+        */
     }
-    else if(x < -0.01f){
-      transform.localScale = new Vector2(-1, 1);
+
+    public void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.tag=="lantai"){
+            isJump = false;
+        }
     }
-    /*if(Input.GetKey(KeyCode.D)){
-      rb.AddForce(Vector2.right);
+    private void OnCollisionExit2D(Collision2D other) {
+        if(other.gameObject.tag == "lantai"){
+            isJump = true;
+        }
     }
-    if(Input.GetKey(KeyCode.A)){
-      rb.AddForce(Vector2.left);
-    }*/
-  }
-  
-  public void OnCollisionEnter2D(Collision2D other){
-    if(other.gameObject.tag == "lantai"){
-      isJump = false;
+
+    void Flip(){
+        FacingRight = !FacingRight;
+        FirePoint.transform.Rotate(0,180,0);
     }
-  }
-  
-  public void OnCollisionExit2D(Collision2D other){
-    if(other.gameObject.tag == "lantai"){
-      isJump = true;
-    }
-  }
 }
